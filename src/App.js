cce09bc10357a919b8524949d28d0e9f8743f680
matthewmjm <matthewmjm@yahoo.com>
2020-12-02 11:42:10 -0700

    sign-up functionality

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react'
 import './App.css';
 import TodoForm from './components/TodoForm'
 import TodoContainer from './components/TodoContainer'
+import SignUpForm from './components/SignUpForm'
 import { patchTodo, postTodo, deleteTodo } from './helpers'
 const todosUrl = "http://localhost:4000/todos/"
+const usersUrl = "http://localhost:4000/users/"
 
 class App extends React.Component {
 
   state = {
-    todos: []
+    todos: [],
+    user: {},
+    alerts: []
   }
 
   componentDidMount(){
@@ -43,10 +47,35 @@ class App extends React.Component {
     deleteTodo(id)
   }
 
+  signUp = (user) => {
+    fetch(usersUrl, {
+      method: "POST",
+      headers: {
+          "Accept": "application/json",
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify({user})
+    })
+    .then(response => response.json())
+    .then(response => {
+      if(response.errors){
+        this.setState({alerts: response.errors})
+      }
+      else {
+        localStorage.setItem('token', response.token)
+        this.setState({
+          user: response.user,
+          alerts: ["User successfully created!"]
+        })
+      }
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <h1>🗣 Todo App 📝</h1>
+        <SignUpForm signUp={this.signUp} alerts={this.state.alerts} />
         <TodoForm  submitAction={this.addTodo} />
         <TodoContainer todos={this.state.todos} deleteTodo={this.deleteTodo} updateTodo={this.updateTodo} />
       </div>
---
+++ src/components/SignUpForm.js
@@ -0,0 +1,39 @@
+import React, { useState} from 'react'
+
+function SignUpForm(props) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        let user = {
+            username,
+            password
+        }
+
+        props.signUp(user)
+    }
+
+    const handleChange = ({target}) => {
+        return target.name === 'username'
+            ? setUsername(target.value)
+            : setPassword(target.value)
+    }
+
+    const showAlerts = () => props.alerts.map(alert => <p>{alert}</p>)
+
+    return (
+        <form className="signup-form" onSubmit={handleSubmit}>
+            <h1>Sign-Up</h1>
+            <label>Username</label>
+            <input type="text" name="username" value={username} onChange={handleChange} />
+            <label>Password</label>
+            <input type="password" name="password" value={password} onChange={handleChange} />
+            <input type="submit" />
+            {props.alerts ? showAlerts() : null }
+        </form>
+    )
+} 
+
+
+export default SignUpForm
